da918b106bc93615bf5cea801056dca4cd1f59be
Denis <60510643+DenchPokepon@users.noreply.github.com>
2024-06-01 03:09:19 +0000

    change

--- superset/docker-entrypoint.sh
+++ superset/docker-entrypoint.sh
@@ -4,30 +4,32 @@
 
 # pip install apache-superset
 
-# create Admin user, you can read these values from env or anywhere else possible
-superset fab create-admin --username "admin" --firstname admin --lastname admin --email "admin@admin.com" --password "admin"
 
 
 
-sudo pip install --upgrade pip
+
+pip install --upgrade pip
 
 curl -sS https://raw.githubusercontent.com/apache/superset/2.0.1/requirements/base.txt | \
        tail -n +10 | \
        awk -v ORS=" " '/^[A-z]/{print}' | \
        xargs pip install apache-superset==2.0.1
 
-sudo pip install clickhouse-connect
+pip install clickhouse-connect
 
 # superset устанавливает кривую версию cryptography и sqlalchemy...
-sudo pip uninstall cryptography
-sudo pip uninstall pyopenssl
-sudo pip install cryptography==3.4.7
-sudo pip install mysqlclient
+pip uninstall cryptography
+pip uninstall pyopenssl
+pip install cryptography==3.4.7
+pip install mysqlclient
 
 # pip install sqlalchemy==1.2.18
 
 # # # Load Examples
 # # superset load_examples
+
+# create Admin user, you can read these values from env or anywhere else possible
+superset fab create-admin --username "admin" --firstname admin --lastname admin --email "admin@admin.com" --password "admin"
 
 # Upgrading Superset metastore
 superset db upgrade
