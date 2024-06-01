4c3824fbdfca00d06cd656607a1f13260c0a2039
Denis <60510643+DenchPokepon@users.noreply.github.com>
2024-06-01 03:33:44 +0000

    ;

--- superset/docker-entrypoint.sh
+++ superset/docker-entrypoint.sh
@@ -12,7 +12,7 @@
 curl -sS https://raw.githubusercontent.com/apache/superset/4.0.1/requirements/base.txt | \
        tail -n +10 | \
        awk -v ORS=" " '/^[A-z]/{print}' | \
-       xargs pip install apache-superset
+       xargs pip install apache-superset --user
 
 pip install clickhouse-connect
 
