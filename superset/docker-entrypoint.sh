138453478805fed4e7ce3bec3d2931d55b070663
Denis <60510643+DenchPokepon@users.noreply.github.com>
2024-06-01 02:27:22 +0000

    change

--- superset/docker-entrypoint.sh
+++ superset/docker-entrypoint.sh
@@ -9,20 +9,20 @@ superset fab create-admin --username "admin" --firstname admin --lastname admin 
 
 
 
-pip install --upgrade pip
+sudo pip install --upgrade pip
 
 curl -sS https://raw.githubusercontent.com/apache/superset/2.0.1/requirements/base.txt | \
        tail -n +10 | \
        awk -v ORS=" " '/^[A-z]/{print}' | \
        xargs pip install apache-superset==2.0.1
 
-pip install clickhouse-connect
+sudo pip install clickhouse-connect
 
 # superset устанавливает кривую версию cryptography и sqlalchemy...
-pip uninstall cryptography
-pip uninstall pyopenssl
-pip install cryptography==3.4.7
-pip install mysqlclient
+sudo pip uninstall cryptography
+sudo pip uninstall pyopenssl
+sudo pip install cryptography==3.4.7
+sudo pip install mysqlclient
 
 # pip install sqlalchemy==1.2.18
 
